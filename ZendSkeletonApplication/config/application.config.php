a866d57f93e96dba7fb8e585714cdf9f5ddc0aba
Jigal Sanders <jigalroecha@gmail.com>
2012-03-17 22:47:44 +0100

    doctrine orm module verwijderd

--- ZendSkeletonApplication/config/application.config.php
+++ ZendSkeletonApplication/config/application.config.php
@@ -3,7 +3,7 @@ return array(
     'modules' => array(
         'Application',
         'DoctrineModule',
-        'DoctrineORMModule'
+//        'DoctrineORMModule'
     ),
     'module_listener_options' => array( 
         'config_cache_enabled' => false,
