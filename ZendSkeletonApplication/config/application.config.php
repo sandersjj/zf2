5e4294fd8e07206d1d9ff5627bd6c8e451aadae4
Jigal Sanders <jigalroecha@gmail.com>
2012-03-21 21:28:48 +0100

    changed files

--- ZendSkeletonApplication/config/application.config.php
+++ ZendSkeletonApplication/config/application.config.php
@@ -3,7 +3,8 @@ return array(
     'modules' => array(
         'Application',
         'DoctrineModule',
-//        'DoctrineORMModule'
+        'DoctrineORMModule',
+        'User'
     ),
     'module_listener_options' => array( 
         'config_cache_enabled' => false,
